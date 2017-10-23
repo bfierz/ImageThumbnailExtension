fa2837c04ecc312fd69bcd639f106627b50435b2
Basil Fierz <basil.fierz@hotmail.com>
2017-10-23 20:06:54 +0200

    Windows treats bitmap colors in reversed order

--- thumbnailprovider.cpp
+++ thumbnailprovider.cpp
@@ -131,9 +131,9 @@ STDMETHODIMP ThumbnailProvider::GetThumbnail(UINT cx,
 	
 	DirectX::ScratchImage bitmap;
 	if (DirectX::IsCompressed(_meta.format))
-		DirectX::Decompress(*_image.GetImage(0, 0, 0), DXGI_FORMAT_R8G8B8A8_UNORM, bitmap);
+		DirectX::Decompress(*_image.GetImage(0, 0, 0), DXGI_FORMAT_B8G8R8A8_UNORM, bitmap);
 	else
-		DirectX::Convert(*_image.GetImage(0, 0, 0), DXGI_FORMAT_R8G8B8A8_UNORM, DirectX::TEX_FILTER_LINEAR, 0, bitmap);
+		DirectX::Convert(*_image.GetImage(0, 0, 0), DXGI_FORMAT_B8G8R8A8_UNORM, DirectX::TEX_FILTER_LINEAR, 0, bitmap);
 	
 	DirectX::ScratchImage thumbnail;
 	DirectX::Resize(*bitmap.GetImage(0, 0, 0), width, height, DirectX::TEX_FILTER_LINEAR, thumbnail);
